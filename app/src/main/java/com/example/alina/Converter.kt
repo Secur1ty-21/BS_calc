c7d0eef6a1cb60c48f0c85c5b9aeaf73d210faf3
Secur1ty-21 <72816249+Secur1ty-21@users.noreply.github.com>
2021-10-08 22:01:05 +0300

    Test_commit

--- app/src/main/java/com/example/alina/Converter.kt
+++ app/src/main/java/com/example/alina/Converter.kt
@@ -13,103 +13,117 @@ class Converter : AppCompatActivity() {
     private var initial : EditText? = null
     private var finite : EditText? = null
     private var buttonTranslate : Button? = null
-    private var calculator : Button? = null
-    private var settings : Button? = null
     private var textAnswer : TextView? = null
+    private var calculator : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_converter)
-        number = findViewById(R.id.numbers)
+        number = findViewById(R.id.number)
         initial = findViewById(R.id.initial)
         finite = findViewById(R.id.finite)
         buttonTranslate = findViewById(R.id.translate)
         textAnswer = findViewById(R.id.answer)
         calculator = findViewById(R.id.button6)
-        settings = findViewById(R.id.setting2)
-        settings?.setOnClickListener {
-            val intent3 = Intent(this, Settings::class.java)
-            startActivity(intent3)
-        }
         calculator?.setOnClickListener {
             val intent2 = Intent(this, Calculator::class.java)
             startActivity(intent2)
+            finish()
         }
         buttonTranslate?.setOnClickListener {
             val num: String = number?.text.toString()
             val startSystem: String = initial?.text.toString()
             val finishSystem: String = finite?.text.toString()
-            val startSystemInt : Int
-            val finishSystemInt : Int
+            var startSystemInt = 0
+            var finishSystemInt = 0
             var len: Int = num.length - 1
-            var answer : Long = 0
+            var answer = 0
             var degree = 0
             var remains = 0
-            var sign = false
             if (num != "" && startSystem != "" && finishSystem != "") {
                 startSystemInt = startSystem.toInt()
                 finishSystemInt = finishSystem.toInt()
-                if (startSystemInt in 2..16  && finishSystemInt in 2..16) {
-                    while (len != -1) {
-                        when {
-                            num[len] == '-' -> sign = true
-                            num[len] == 'A' -> remains = 10
-                            num[len] == 'B' -> remains = 11
-                            num[len] == 'C' -> remains = 12
-                            num[len] == 'D' -> remains = 13
-                            num[len] == 'E' -> remains = 14
-                            num[len] == 'F' -> remains = 15
-                            num[len] == '1' -> remains = 1
-                            num[len] == '2' -> remains = 2
-                            num[len] == '3' -> remains = 3
-                            num[len] == '4' -> remains = 4
-                            num[len] == '5' -> remains = 5
-                            num[len] == '6' -> remains = 6
-                            num[len] == '7' -> remains = 7
-                            num[len] == '8' -> remains = 8
-                            num[len] == '9' -> remains = 9
-                            num[len] == '0' -> remains = 0
-                        }
-                        answer += remains * startSystemInt.toDouble().pow(degree.toDouble()).toLong()
-                        len--
-                        degree++
-                    }
-                    var save = answer
-                    var result1 = ""
-                    while (save > 0) {
-                        remains = (save % finishSystemInt).toInt()
-                        when (remains) {
-                            10 -> result1 += "A"
-                            11 -> result1 += "B"
-                            12 -> result1 += "C"
-                            13 -> result1 += "D"
-                            14 -> result1 += "E"
-                            15 -> result1 += "F"
-                            1 -> result1 += "1"
-                            2 -> result1 += "2"
-                            3 -> result1 += "3"
-                            4 -> result1 += "4"
-                            5 -> result1 += "5"
-                            6 -> result1 += "6"
-                            7 -> result1 += "7"
-                            8 -> result1 += "8"
-                            9 -> result1 += "9"
-                            0 -> result1 += "0"
-                        }
-                        save /= finishSystemInt
-                    }
-                    if (sign)
-                        result1 += "-"
-                    result1 = result1.reversed()
-                    val zer0: Long = 0
-                    if (answer == zer0)
-                        result1 = "0"
-                    textAnswer?.text = result1
-                }
-                else
-                    textAnswer?.text = getString(R.string.system)
             } else
                 textAnswer?.text = getString(R.string.error)
+            while (len != -1) {
+                if (num[len] == 'A')
+                    remains = 10
+                if (num[len] == 'B')
+                    remains = 11
+                if (num[len] == 'C')
+                    remains = 12
+                if (num[len] == 'D')
+                    remains = 13
+                if (num[len] == 'E')
+                    remains = 14
+                if (num[len] == 'F')
+                    remains = 15
+                if (num[len] == '1')
+                    remains = 1
+                if (num[len] == '2')
+                    remains = 2
+                if (num[len] == '3')
+                    remains = 3
+                if (num[len] == '4')
+                    remains = 4
+                if (num[len] == '5')
+                    remains = 5
+                if (num[len] == '6')
+                    remains = 6
+                if (num[len] == '7')
+                    remains = 7
+                if (num[len] == '8')
+                    remains = 8
+                if (num[len] == '9')
+                    remains = 9
+                if (num[len] == '0')
+                    remains = 0
+                answer += remains * startSystemInt.toDouble().pow(degree.toDouble()).toInt()
+                len--
+                degree++
+            }
+            var save = answer
+            var result1 = ""
+            while (save > 0) {
+                remains = save % finishSystemInt
+                if (remains == 10)
+                    result1 += "A"
+                if (remains == 11)
+                    result1 += "B"
+                if (remains == 12)
+                    result1 += "C"
+                if (remains == 13)
+                    result1 += "D"
+                if (remains == 14)
+                    result1 += "E"
+                if (remains == 15)
+                    result1 += "F"
+                if (remains == 1)
+                    result1 += "1"
+                if (remains == 2)
+                    result1 += "2"
+                if (remains == 3)
+                    result1 += "3"
+                if (remains == 4)
+                    result1 += "4"
+                if (remains == 5)
+                    result1 += "5"
+                if (remains == 6)
+                    result1 += "6"
+                if (remains == 7)
+                    result1 += "7"
+                if (remains == 8)
+                    result1 += "8"
+                if (remains == 9)
+                    result1 += "9"
+                if (remains == 0)
+                    result1 += "0"
+                save /= finishSystemInt
+            }
+            result1 = result1.reversed()
+            if (answer == 0)
+                result1 = "0"
+            textAnswer?.text = result1
         }
     }
 }
